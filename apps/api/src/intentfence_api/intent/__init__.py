123e907df28967ce8b1615fbb52db0291c1ccf0e
Rajeet <112367119+rajeet-04@users.noreply.github.com>
2026-08-22 17:19:12 +0530

    feat(phase5): export intent compiler API

---
+++ apps/api/src/intentfence_api/intent/__init__.py
@@ -0,0 +1,7 @@
+from .compiler import IntentContractDraft, compile_intent_contract, revise_intent_contract
+
+__all__ = [
+    "IntentContractDraft",
+    "compile_intent_contract",
+    "revise_intent_contract",
+]
